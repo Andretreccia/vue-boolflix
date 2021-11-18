8415045c786adc853c1159df1c654c95ebbf207b
Andretreccia <andreabtreccia@gmail.com>
2021-11-18 14:39:54 +0100

    add in the DOM the films related to the user's search

--- src/App.vue
+++ src/App.vue
@@ -19,4 +19,5 @@ export default {
 </script>
 
 <style lang="scss">
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
 </style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,7 +1,28 @@
 <template>
   <div>
     <input v-model="userInput" type="text" placeholder="cerca un film" />
-    <button @click="debugFunction">search</button>
+    <button @click="callSearch">search</button>
+    <div class="container">
+      <div class="row justify-content-center py-5">
+        <div class="col col-5" v-for="film in films" :key="film.qualcosa">
+          <div class="card p-3">
+            <h4 class="text-center">Film:</h4>
+            <div class="title">
+              <span>Titolo: {{ film.title }}</span>
+            </div>
+            <div class="original_title">
+              <span>Titolo originale: {{ film.original_title }}</span>
+            </div>
+            <div class="lingua">
+              <span>Lingua: {{ film.original_language }}</span>
+            </div>
+            <div class="voto">
+              <span>Voto: {{ film.vote_average }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,19 +31,15 @@ import axios from "axios";
 export default {
   data() {
     return {
-      film: [],
+      films: [],
       loading: false,
       apiKey: "cde5362f29f305e58d2e01a906c2d55",
       userInput: "",
     };
   },
-  mounted() {
-    this.callFilmApi();
-  },
+  mounted() {},
   methods: {
-    callFilmApi() {},
-
-    debugFunction() {
+    callSearch() {
       axios
         .get(
           "https://api.themoviedb.org/3/search/movie?api_key=" +
@@ -32,7 +49,7 @@ export default {
             "&page=1&include_adult=false"
         )
         .then((r) => {
-          this.film = r.data.results;
+          this.films = r.data.results;
           this.loading = true;
         })
         .catch((e) => {
