7916d825e70e8a99c404c9e789b46c425c4012f0
Andretreccia <andreabtreccia@gmail.com>
2021-11-18 16:30:31 +0100

    Create separated component for the film list element

---
+++ src/components/ResponseFilmComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="row justify-content-center py-5">
+    <div class="col col-5" v-for="film in films" :key="film.id">
+      <div class="card p-3 bg-dark bg-opacity-10 mt-4">
+        <h4 class="text-center">{{ film.title }}</h4>
+        <div class="title">
+          <span>Titolo: {{ film.title }}</span>
+        </div>
+        <div class="original_title">
+          <span>Titolo originale: {{ film.original_title }}</span>
+        </div>
+        <div class="lingua">
+          <span>Lingua: {{ film.original_language }}</span>
+        </div>
+        <div class="voto">
+          <span>Voto: {{ film.vote_average }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    films: Array,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -6,32 +6,18 @@
       <button @click="callSearch">search</button>
     </div>
     <div class="container">
-      <div class="row justify-content-center py-5">
-        <div class="col col-5" v-for="film in films" :key="film.id">
-          <div class="card p-3 bg-dark bg-opacity-10 mt-4">
-            <h4 class="text-center">{{ film.title }}</h4>
-            <div class="title">
-              <span>Titolo: {{ film.title }}</span>
-            </div>
-            <div class="original_title">
-              <span>Titolo originale: {{ film.original_title }}</span>
-            </div>
-            <div class="lingua">
-              <span>Lingua: {{ film.original_language }}</span>
-            </div>
-            <div class="voto">
-              <span>Voto: {{ film.vote_average }}</span>
-            </div>
-          </div>
-        </div>
-      </div>
+      <ResponseFilmComponent :films="films" />
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import ResponseFilmComponent from "./ResponseFilmComponent.vue";
 export default {
+  components: {
+    ResponseFilmComponent,
+  },
   data() {
     return {
       films: [],
